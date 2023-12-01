159a2f89c01d1f274df8e88caf51d83cf79aaf4e
“Faraz <afaraz@umich.edu>
2023-11-30 20:01:22 -0500

    Initial commit: Added SelectionSort.java and testSelectionSort.java

---
+++ testSelectionSort.java
@@ -0,0 +1,46 @@
+import org.junit.Test; 
+import java.util.Arrays;
+
+public class testSelectionSort {      
+	SelectionSort sort = new SelectionSort();      
+	@Test     
+	public void testPositive(){         
+		int[] arr = new int[]{8,9,7,10,2};         
+		int[] res = new int[]{2,7,8,9,10};         
+		sort.basicSelectionSort(arr);  
+		System.out.println("Test Positive:");
+        System.out.println("Actual   : " + Arrays.toString(arr));
+        System.out.println("Expected : " + Arrays.toString(res));
+		assert(Arrays.equals(arr,res));     
+		}      
+	@Test     
+	public void testNegatives(){         
+		int[] arr = new int[]{-8,-9,-7,-10,-2};         
+		int[] res = new int[]{-10,-9,-8,-7,-2};         
+		sort.basicSelectionSort(arr);  
+		System.out.println("Test Negatives:");
+        System.out.println("Actual   : " + Arrays.toString(arr));
+        System.out.println("Expected : " + Arrays.toString(res));
+		assert(Arrays.equals(arr,res));     
+		}     
+	@Test     
+	public void testMixed(){         
+		int[] arr = new int[]{-8,-9,7,-10,2,0};         
+		int[] res = new int[]{-10,-9,-8,0,2,7};         
+		sort.basicSelectionSort(arr); 
+		System.out.println("Test Mixed:");
+        System.out.println("Actual   : " + Arrays.toString(arr));
+        System.out.println("Expected : " + Arrays.toString(res));
+		assert(Arrays.equals(arr,res));     
+		}      
+	@Test     
+	public void testDuplicates(){         
+		int[] arr = new int[]{-8,-9,7,2,-8,2,-10,2,0};         
+		int[] res = new int[]{-10,-9,-8,-8,0,2,2,2,7};         
+		sort.basicSelectionSort(arr);   
+		System.out.println("Test Duplicates:");
+        System.out.println("Actual   : " + Arrays.toString(arr));
+        System.out.println("Expected : " + Arrays.toString(res));
+		assert(Arrays.equals(arr,res));     
+		} 
+	}
