203fae9012e8b0634341b97298e27ba699c314fd
“Faraz <afaraz@umich.edu>
2023-12-14 16:41:51 -0500

    Inital Commit Bro

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,15 +1,8 @@
 public class SelectionSort {
 	private int temp;
-	/** Creates a new instance of SelectionSort */
 	public SelectionSort() {
 	}
-/* A simple SelectionSort algorithm
-* pre-condition:
-* post-condition:
-* inputs:
-* outputs:
-* special conditions:
-*/
+
 	public int[] basicSelectionSort(int[] x) {
 		for (int i = 0; i < x.length; ++i) {
 			for (int j= i+1; j < x.length; ++j) {
@@ -18,8 +11,8 @@ public class SelectionSort {
 					x[i] = x[j];
 					x[j] = temp;
 				}
-			} // end of inner for loop
-		} // end of outer for loop
+			} 
+		} 
 		return x;
-	} // end of basicSelectionSort method
+	} 
 }
--- testSelectionSort.java
+++ testSelectionSort.java
@@ -33,14 +33,5 @@ public class testSelectionSort {
         System.out.println("Expected : " + Arrays.toString(res));
 		assert(Arrays.equals(arr,res));     
 		}      
-	@Test     
-	public void testDuplicates(){         
-		int[] arr = new int[]{-8,-9,7,2,-8,2,-10,2,0};         
-		int[] res = new int[]{-10,-9,-8,-8,0,2,2,2,7};         
-		sort.basicSelectionSort(arr);   
-		System.out.println("Test Duplicates:");
-        System.out.println("Actual   : " + Arrays.toString(arr));
-        System.out.println("Expected : " + Arrays.toString(res));
-		assert(Arrays.equals(arr,res));     
-		} 
+	//branch1edit
 	}
